e5fffd14a786359ee4c504dd57eb3e17221e15c3
Karla <karlaabdraeva@icloud.com>
2025-02-02 16:28:27 +0300

    Переименовываю файл, расставляю некоторые элементы по файлам

--- js/create-posts-array.js
+++ js/create-posts-array.js
@@ -1,19 +1,19 @@
-import { getDataPosts } from './data.js';
-import { getRandomInteger, generateRandomPostId, generateRandomPhotoId, generateRandomCommentId } from './utils.js';
+import { getDataPosts } from './posts-data.js';
+import { getRandomInteger, getRandomIdFromRangeGenerator } from './utils.js';
+
+const POST_ID_SET = { MIN: 1, MAX: 25 };
+const PHOTO_ID_SET = { MIN: 1, MAX: 25 };
+const COMMENT_ID_SET = { MIN: 1, MAX: 10000 };
+
+const generateRandomPostId = getRandomIdFromRangeGenerator(POST_ID_SET.MIN, POST_ID_SET.MAX);
+const generateRandomPhotoId = getRandomIdFromRangeGenerator(PHOTO_ID_SET.MIN, PHOTO_ID_SET.MAX);
+const generateRandomCommentId = getRandomIdFromRangeGenerator(COMMENT_ID_SET.MIN, COMMENT_ID_SET.MAX);
 
 const POSTS_ARRAY_LENGTH = 25;
-const AVATARS_SET = {
-  MIN: 1,
-  MAX: 6,
-};
-const LIKES_SET = {
-  MIN: 15,
-  MAX: 200,
-};
-const COMMENTS_SET = {
-  MIN: 0,
-  MAX: 30,
-};
+
+const AVATARS_SET = { MIN: 1, MAX: 6 };
+const LIKES_SET = { MIN: 15, MAX: 200 };
+const COMMENTS_SET = { MIN: 0, MAX: 30 };
 
 const { DESCRIPTIONS, COMMENTS, NAMES } = getDataPosts();
 
