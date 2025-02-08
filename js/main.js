bae9fa5f4bbf8d2204a8028e56f5bd60023126ff
Karla <karlaabdraeva@icloud.com>
2025-02-08 15:32:27 +0300

    Задание 8.14 открывается и закрывается

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { posts } from './create-posts-array.js';
 import { renderMiniatures } from './create-miniatures.js';
+import { showBigPicture } from './show-big-picture.js';
 
 const createdArray = posts;
 renderMiniatures(createdArray);
+showBigPicture(createdArray[0]);
